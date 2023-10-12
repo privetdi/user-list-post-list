a3459a69d77892a76723968a2549fd5ed2ca850e
Dmitriy <dmleshi49@gmail.com>
2023-10-12 16:18:25 +0400

    gh-page.0.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from "react";
-import { Routes, Route, Link, Navigate } from "react-router-dom";
-import NavBar from "./components/navBar";
-import "./App.css";
-import NewEmploye from "./pages/user/newEmploye";
-import NewDepartment from "./pages/post/newDepartment";
-import TableEmploye from "./pages/user/tableEmploye.";
-import TablePosts from "./pages/post/tablePosts";
-import ChangeUser from "./pages/user/changeUses";
-import ViewUser from "./pages/user/viewUser";
-import ChangePost from "./pages/post/changePost";
-import ViewPost from "./pages/post/viewPost";
-import NewPost from "./pages/post/newPost";
+import React, { useEffect } from 'react'
+import { Routes, Route, Link, Navigate } from 'react-router-dom'
+import NavBar from './components/navBar'
+import './App.css'
+import NewEmploye from './pages/user/newEmploye'
+import NewDepartment from './pages/post/newDepartment'
+import TableEmploye from './pages/user/tableEmploye.'
+import TablePosts from './pages/post/tablePosts'
+import ChangeUser from './pages/user/changeUses'
+import ViewUser from './pages/user/viewUser'
+import ChangePost from './pages/post/changePost'
+import ViewPost from './pages/post/viewPost'
+import NewPost from './pages/post/newPost'
+
+const githubPages = '/user-list-post-list'
 
 function App() {
   return (
@@ -18,23 +20,20 @@ function App() {
       <NavBar />
       <div className="App__wrapper">
         <Routes>
-          <Route path="/" element={<Navigate to="/posts" />} />
-          <Route
-            path="/user-list-post-list/"
-            element={<Navigate to="/posts" />}
-          />
-          <Route path="/users" element={<TableEmploye />} />
-          <Route path="/posts" element={<TablePosts />} />
-          <Route path="/posts/change/:post" element={<ChangePost />} />
-          <Route path="/users/change/:user" element={<ChangeUser />} />;
-          <Route path="/users/сreation" element={<NewEmploye />} />;
-          <Route path="/posts/сreation" element={<NewPost />} />;
-          <Route path="/users/view/:user" element={<ViewUser />} />;
-          <Route path="/post/view/:post" element={<ViewPost />} />;
+          <Route path="/" element={<Navigate to={`${githubPages}/posts`} />} />
+          <Route path={`${githubPages}`} element={<Navigate to={`${githubPages}/posts`} />}/>
+          <Route path={`${githubPages}/users`} element={<TableEmploye />} />
+          <Route path={`${githubPages}/posts`} element={<TablePosts />} />
+          <Route path={`${githubPages}/posts/change/:post`} element={<ChangePost />} />
+          <Route path={`${githubPages}/users/change/:user`} element={<ChangeUser />}/>
+          <Route path={`${githubPages}/users/сreation`} element={<NewEmploye />}/>
+          <Route path={`${githubPages}/posts/сreation`} element={<NewPost />} />
+          <Route path={`${githubPages}/users/view/:user`} element={<ViewUser />}/>
+          <Route path={`${githubPages}/post/view/:post`} element={<ViewPost />}/>
         </Routes>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
